77279801a6e7127df3f3be2450c9335ec34d16cc
Julien Stroheker <juliens@microsoft.com>
2020-04-03 13:49:33 -0400

    Add license to billing.go

--- pkg/util/billing/billing.go
+++ pkg/util/billing/billing.go
@@ -1,5 +1,8 @@
 package billing
 
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
 import (
 	"bytes"
 	"context"
@@ -19,7 +22,7 @@ const (
 	tenantIDAME  string = "33e01921-4d64-4f8c-a055-5bdaffd5e33d"
 )
 
-// isSubscriptionRegisteredToE2E returns true if the subscription is having the
+// IsSubscriptionRegisteredToE2E returns true if the subscription is having the
 // "Microsoft.ContainerService/SaveAROTestConfig" feature registered
 func IsSubscriptionRegisteredToE2E(sub *api.SubscriptionProperties) bool {
 	if sub.TenantID == tenantIDMSFT || sub.TenantID == tenantIDAME {
@@ -32,6 +35,8 @@ func IsSubscriptionRegisteredToE2E(sub *api.SubscriptionProperties) bool {
 	return false
 }
 
+// CreateOrUpdateE2Eblob create a copy of the billing document into the e2e storage account.
+// This is used later on for E2E Billing
 func CreateOrUpdateE2Eblob(ctx context.Context, env env.Interface, doc api.BillingDocument, resource azure.Resource) error {
 	key, err := env.E2EStorageAccountKey(ctx)
 	if err != nil {
